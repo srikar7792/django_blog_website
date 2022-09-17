69bc173c4f499f057b42a20ee8f2c7847a8f4a4d
Srikar Perala <srikarperala@adsl-172-10-100-75.dsl.hstntx.sbcglobal.net>
2022-09-17 17:36:45 +0700

    Added Templates and setup base html

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1> Hello World! </h1>")
+    my_title = "Hello there!!!"
+    return render(request, "hello_world.html", {"title":my_title})
+
+def about_page(request):
+    return render(request, "about.html", {"title":"About Us!!!"})
+
+def contact_page(request):
+    return render(request, "hello_world.html", {"title":"Contact Us!!!"})
